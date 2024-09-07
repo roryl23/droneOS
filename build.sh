e620c219650da986a5b7071f2495cc2c567c4520
Rory Linehan <rorylinehan@protonmail.com>
2024-09-07 18:29:37 -0500

    refactor build script by grouping things better

--- build.sh
+++ build.sh
@@ -22,12 +22,13 @@ RPI_LINUX_BRANCH=rpi-6.6.y
 SD_CARD_BOOT="${SD_CARD}1"
 SD_CARD_ROOT="${SD_CARD}2"
 
+# don't need firmware if we're using provided images
 # get raspberry pi firmware
-if ! [ -d "build/firmware" ]; then
-  cd build && \
-  git clone --depth=1 git@github.com:raspberrypi/firmware.git
-  cd $PROJECT_DIR
-fi
+#if ! [ -d "build/firmware" ]; then
+#  cd build && \
+#  git clone --depth=1 git@github.com:raspberrypi/firmware.git
+#  cd $PROJECT_DIR
+#fi
 
 # get kernel source and configure
 if ! [ -d "build/linux" ]; then
@@ -35,7 +36,7 @@ if ! [ -d "build/linux" ]; then
   cd build && \
   git clone --depth=1 https://github.com/raspberrypi/linux && \
   git checkout $RPI_LINUX_BRANCH
-  cd $PROJECT_DIR
+  cd "$PROJECT_DIR"
 fi
 # get real time kernel patch
 if ! [ -d "build/patches" ]; then
@@ -48,7 +49,7 @@ if ! [ -d "build/patches" ]; then
   cd linux && \
   git am -3 ../patches/*
   git am --skip
-  cd $PROJECT_DIR
+  cd "$PROJECT_DIR"
 fi
 
 # set up sd card
@@ -90,28 +91,23 @@ if ! [ -f "$IMAGE_FILE" ]; then
 fi
 sudo dd bs=1M if="$IMAGE_FILE" of=/dev/"${SD_CARD}" status=progress
 
-# build kernel
-if [ -d "build/linux" ]; then
-  cd build/linux && \
-  # configure
-  cp "$PROJECT_DIR"/configs/.config . && \
-  mkdir -p mnt/rpi_boot && \
-  mkdir -p mnt/rpi_root && \
-  sudo mount /dev/"${SD_CARD_BOOT}" mnt/rpi_boot && \
-  sudo mount /dev/"${SD_CARD_ROOT}" mnt/rpi_root && \
-  sudo mkdir -p mnt/rpi_boot/overlays/ && \
-  sudo cp "$PROJECT_DIR"/configs/config-"${KERNEL}".txt mnt/rpi_boot
-  # set up user to avoid booting into userconfig on first boot
-  PASSWORD_ENCRYPTED=$(echo "$USER_PASSWORD" | openssl passwd -6 -stdin)
-  echo "${USER_NAME}:${PASSWORD_ENCRYPTED}" | sudo tee mnt/rpi_boot/userconf.txt
-  # set up wifi network
-  if [[ $TYPE == "base" ]]; then
-    sudo mkdir -p mnt/rpi_root/home/"${USER_NAME}"/droneOS && \
-    echo "$SSID" | sudo tee mnt/rpi_root/home/"${USER_NAME}"/droneOS/ssid && \
-    echo "$SSID_PASSWORD" | sudo tee mnt/rpi_root/home/"${USER_NAME}"/droneOS/ssid_password
-  elif [[ $TYPE == "drone" ]]; then
-    sudo mkdir -p mnt/rpi_root/etc/NetworkManager/system-connections && \
-    echo "
+# filesystem configuration
+cd build/linux && \
+mkdir -p mnt/rpi_boot && \
+mkdir -p mnt/rpi_root && \
+sudo mount /dev/"${SD_CARD_BOOT}" mnt/rpi_boot && \
+sudo mount /dev/"${SD_CARD_ROOT}" mnt/rpi_root && \
+# set up user to avoid booting into userconfig on first boot
+PASSWORD_ENCRYPTED=$(echo "$USER_PASSWORD" | openssl passwd -6 -stdin)
+echo "${USER_NAME}:${PASSWORD_ENCRYPTED}" | sudo tee mnt/rpi_boot/userconf.txt
+# set up wifi network
+if [[ $TYPE == "base" ]]; then
+  sudo mkdir -p mnt/rpi_root/home/"${USER_NAME}"/droneOS && \
+  echo "$SSID" | sudo tee mnt/rpi_root/home/"${USER_NAME}"/droneOS/ssid && \
+  echo "$SSID_PASSWORD" | sudo tee mnt/rpi_root/home/"${USER_NAME}"/droneOS/ssid_password
+elif [[ $TYPE == "drone" ]]; then
+  sudo mkdir -p mnt/rpi_root/etc/NetworkManager/system-connections && \
+  echo "
 [connection]
 id=${SSID}
 uuid=
@@ -134,16 +130,24 @@ method=auto
 [ipv6]
 method=auto
 " | sudo tee mnt/rpi_root/etc/NetworkManager/system-connections/"${SSID}".nmconnection && \
-    sudo chmod -Rv 600 mnt/rpi_root/etc/NetworkManager/system-connections/"${SSID}".nmconnection && \
-    sudo chown -Rv root:root mnt/rpi_root/etc/NetworkManager/system-connections/"${SSID}".nmconnection
-  fi
-  # add scripts to base and set final permissions
-  if [[ $TYPE == "base" ]]; then
-    sudo mkdir -p mnt/rpi_root/home/"${USER_NAME}"/droneOS/scripts && \
-    sudo cp "$PROJECT_DIR"/scripts/* mnt/rpi_root/home/"${USER_NAME}"/droneOS/ && \
-    sudo chown -Rv "${USER_NAME}":"${USER_NAME}" mnt/rpi_root/home/"${USER_NAME}"
-  fi
+  sudo chmod -Rv 600 mnt/rpi_root/etc/NetworkManager/system-connections/"${SSID}".nmconnection && \
+  sudo chown -Rv root:root mnt/rpi_root/etc/NetworkManager/system-connections/"${SSID}".nmconnection
+fi
+# add scripts to base and set final permissions
+if [[ $TYPE == "base" ]]; then
+  sudo mkdir -p mnt/rpi_root/home/"${USER_NAME}"/droneOS/scripts && \
+  sudo cp "$PROJECT_DIR"/scripts/* mnt/rpi_root/home/"${USER_NAME}"/droneOS/ && \
+  sudo chown -Rv "${USER_NAME}":"${USER_NAME}" mnt/rpi_root/home/"${USER_NAME}"
+fi
+cd "$PROJECT_DIR"
 
+# build kernel
+if [ -d "build/linux" ]; then
+  # add configs for kernel build
+  cd build/linux && \
+  cp "$PROJECT_DIR"/configs/.config . && \
+  sudo mkdir -p mnt/rpi_boot/overlays/ && \
+  sudo cp "$PROJECT_DIR"/configs/config-"${KERNEL}".txt mnt/rpi_boot
   # compile and install kernel
   if [[ "$KERNEL" == "kernel" ]]; then
     make -j${THREADS} KERNEL="$KERNEL" ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- bcmrpi_defconfig && \
@@ -169,6 +173,7 @@ method=auto
     sudo cp arch/arm64/boot/dts/overlays/README mnt/rpi_boot/overlays/
   fi
 
+  # don't need firmware if we're using provided images
   # install firmware
 #  sudo cp $PROJECT_DIR/build/firmware/boot/start*.elf mnt/rpi_boot/ && \
 #  sudo cp $PROJECT_DIR/build/firmware/boot/fixup*.dat mnt/rpi_boot/ && \
@@ -177,12 +182,12 @@ method=auto
   # cleanup
   sudo umount /dev/"${SD_CARD_BOOT}"
   sudo umount /dev/"${SD_CARD_ROOT}"
-  cd $PROJECT_DIR
+  cd "$PROJECT_DIR"
 fi
 
 # install source libraries
 cd lib && \
 bash install.sh && \
-cd $PROJECT_DIR
+cd "$PROJECT_DIR"
 
 # build droneOS
