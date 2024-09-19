d5c8d73beb9233746d72b72dcd4313ba36b5edef
Rory Linehan <rorylinehan@protonmail.com>
2024-09-19 09:46:35 -0500

    fix cross compile for ARM64

--- build.sh
+++ build.sh
@@ -1,17 +1,29 @@
 #!/usr/bin/env bash
 
 ARCH=${1:-"arm64"}
+if [[ $ARCH == "arm64" ]]; then
+  CC=aarch64-linux-gnu-gcc
+  ARM=8
+else
+  # gnueabihf?
+  CC=arm-linux-gnueabi-gcc
+  ARM=5
+fi
 
 go mod tidy -e && \
 # TODO: this can be done automatically for all directories in internal/plugin
 env \
+CC="$CC" \
 CGO_ENABLED=1 \
 GOOS=linux \
 GOARCH="$ARCH" \
+GOARM="$ARM" \
 go build -buildmode=plugin -o plugin_droneos.so ./internal/plugin/droneos && \
 env \
+CC="$CC" \
 CGO_ENABLED=1 \
 GOOS=linux \
 GOARCH="$ARCH" \
+GOARM="$ARM" \
 go build -o droneOS.bin ./cmd/droneOS && \
 chmod +x droneOS.bin
