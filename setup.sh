6946e4eabd5eda44faad01c8ea9984307a16e4b9
Rory Linehan <rorylinehan@protonmail.com>
2024-09-29 12:26:27 -0500

    Add joystick dependencies installation steps to setup script

--- setup.sh
+++ setup.sh
@@ -15,3 +15,15 @@ sudo apt install -y \
 
 # create build directory
 mkdir -p build/droneOS
+
+# install joystick dependencies
+cd build
+if [[ ! -f "SDL2-2.0.8.tar.gz" ]]; then
+  wget https://www.libsdl.org/release/SDL2-2.0.8.tar.gz
+fi
+if [[ ! -d "SDL2-2.0.8" ]]; then
+  tar -zxvf SDL2-2.0.8.tar.gz
+fi
+cd SDL2-2.0.8/ && \
+./configure && make && sudo make install && \
+cd ../..
